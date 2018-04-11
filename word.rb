0e14114f0a3ce8ccf5e0236e1edd6c26f9479360
Edmund <edmundcjj@gmail.com>
2018-04-11 10:19:35 +0800

    Completed bonus 1

--- word.rb
+++ word.rb
@@ -6,20 +6,23 @@ def generate_piglatin(word)
   if word.start_with?('a', 'e', 'i', 'o', 'u')
     puts "Word starts with a vowel"
     word += 'way'
-  else
-    puts "Word starts with a consonant"
-    consonant = word[0]
-    word[0] = ""
-    word = word + consonant + "ay"
   end
 
-  return word
-  # vowels.each do |vowel|
-  #   if vowel == a[0]
-  #     word += "way"
-  #   end
-  # end
-  # return word
+  if word.start_with?('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
+    puts "Word starts with a consonant"
+    if word[0] == 'y'
+      consonant = word[0]
+      word[0] = ""
+      word = word + consonant + "ay"
+    else
+      puts "Word contains letter y"
+      first_part = word[0..(word.index('y')-1)]
+      word.slice! first_part
+      first_part += "ay"
+      word += first_part
+    end
+  end
+  
 end
 
-puts generate_piglatin('happy')
+puts generate_piglatin('rhythm')
